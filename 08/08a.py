41e0ed388345241665f30ac7d16da51508bd6c6c
Jesse Kearns <kurnsey@gmail.com>
2017-12-07 23:28:06 -0600

    Day 8

--- 08/08a.py
+++ 08/08a.py
@@ -1,5 +1,37 @@
-inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\06\\input06.txt"
+inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\08\\input08.txt"
 with open(inputfile) as f:
     lines = f.readlines()
+registers = {}
 
-print(lines)
+for line in lines:
+    tokens = line.replace("\n", "").split(" ")
+    regName = tokens[0]
+    addVal = int(tokens[2])
+    if (tokens[1] == "dec"):
+        addVal *= -1
+    condName = tokens[4]
+    cond = tokens[5]
+    condVal = int(tokens[6])
+
+    if (condName not in registers):
+        registers[condName] = 0
+    if (regName not in registers):
+        registers[regName] = 0
+    
+    if  (cond == "==" and registers[condName] == condVal ) or \
+        (cond == "<=" and registers[condName] <= condVal ) or \
+        (cond == ">=" and registers[condName] >= condVal ) or \
+        (cond == "!=" and registers[condName] != condVal ) or \
+        (cond == ">" and registers[condName] > condVal ) or \
+        (cond == "<" and registers[condName] < condVal ):
+        registers[regName] += addVal
+
+max = -999
+for key in registers:
+    val = registers[key]
+    print("{0}: {1}".format(val, key))
+    if val > max:
+        max = val
+
+
+print("\nMax: {0}".format(max))
--- 08/08b.py
+++ 08/08b.py
@@ -1,5 +1,36 @@
-inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\06\\input06.txt"
+inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\08\\input08.txt"
 with open(inputfile) as f:
     lines = f.readlines()
+registers = {}
+max = -999
 
-print(lines)
+for line in lines:
+    tokens = line.replace("\n", "").split(" ")
+    regName = tokens[0]
+    addVal = int(tokens[2])
+    if (tokens[1] == "dec"):
+        addVal *= -1
+    condName = tokens[4]
+    cond = tokens[5]
+    condVal = int(tokens[6])
+
+    if (condName not in registers):
+        registers[condName] = 0
+    if (regName not in registers):
+        registers[regName] = 0
+    
+    if  (cond == "==" and registers[condName] == condVal ) or \
+        (cond == "<=" and registers[condName] <= condVal ) or \
+        (cond == ">=" and registers[condName] >= condVal ) or \
+        (cond == "!=" and registers[condName] != condVal ) or \
+        (cond == ">" and registers[condName] > condVal ) or \
+        (cond == "<" and registers[condName] < condVal ):
+        registers[regName] += addVal
+    
+    if (registers[regName] > max):
+        max = registers[regName]
+
+
+
+
+print("\nMax: {0}".format(max))
