c2cdba6c24a751104473dc5b0ded201b872a3672
Jesse Kearns <kurnsey@gmail.com>
2017-12-09 00:08:22 -0600

    Day 9

---
+++ 09/09a.py
@@ -0,0 +1,34 @@
+inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\09\\input09.txt"
+with open(inputfile) as f:
+    lines = f.readlines()
+stream = lines[0]
+
+totalScore = 0
+currentScore = 0
+isGarbage = False
+shouldIgnore = False
+
+for character in stream:
+    if (shouldIgnore): 
+        shouldIgnore = False
+        continue
+    if (character == "!"):
+        shouldIgnore = True
+        continue
+    if (isGarbage):
+        if character == ">":
+            isGarbage = False
+            continue
+        else : continue
+    if character == "<":
+        isGarbage = True
+    if character == "{":
+        currentScore += 1
+    if character == "}":
+        totalScore += currentScore
+        currentScore -= 1
+
+
+
+
+print(totalScore)
