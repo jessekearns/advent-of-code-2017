7b4bf03929a4d97cd6ac1c36fb9c28c96c711abb
Jesse Kearns <kurnsey@gmail.com>
2017-12-15 23:58:18 -0600

    16

--- 16/16a.py
+++ 16/16a.py
@@ -1,3 +1,40 @@
+def spin(arr, num):
+    standing = arr[0:(16-num)]
+    moving = arr[16-num:16]
+    return moving + standing
+def swap(arr, first, second):
+    temp = arr[first]
+    arr[first] = arr[second]
+    arr[second] = temp
+    return arr
+def partner(arr, first, second):
+    firstIndex = 0
+    secondIndex = 0
+    for i in range(16):
+        if (first == arr[i]):
+            firstIndex = i
+        if (second == arr[i]):
+            secondIndex = i
+    return swap(arr, firstIndex, secondIndex)
+
 inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\16\\input16.txt"
 with open(inputfile) as f:
-    lines = f.readlines()
+    moves = f.readlines()[0].split(',')
+arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+for move in moves:
+    if move[0] == 's':
+        num = int(move[1:])
+        arr = spin(arr, num)
+    elif move[0] == 'x':
+        first = int(move[1:].split('/')[0])
+        second = int(move[1:].split('/')[1])
+        arr = swap(arr, first, second)
+    elif move[0] == 'p':
+        first = move[1:].split('/')[0]
+        second = move[1:].split('/')[1]
+        arr = partner(arr, first, second)
+
+output = ''
+for p in arr: 
+    output += p
+print(output)
