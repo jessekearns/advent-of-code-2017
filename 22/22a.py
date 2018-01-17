1288af3be58d523464aa150feec7644c37074d2f
Jesse Kearns <kurnsey@gmail.com>
2018-01-16 22:59:30 -0600

    22a - bug in 22b

--- 22/22a.py
+++ 22/22a.py
@@ -1,3 +1,51 @@
-inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\20\\input20.txt"
+totalBursts = 10000
+inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\22\\input22.txt"
 with open(inputfile) as f:
     lines = f.readlines()
+
+map = {}
+length = len(lines)
+for i in range(length):
+    for j in range(length):
+        infected = (list(lines[i])[j] == '#')
+        map[(i, j)] = infected
+
+iA = int(length/2)
+jA = int(length/2)
+direction = 1 # Up
+infections = 0
+for iteration in range(totalBursts):
+    if not (iA, jA) in map:
+        map[(iA, jA)] = False
+    infected = map[(iA, jA)]
+
+    # Turn
+    if infected:
+        # right
+        direction += 3
+        direction %= 4
+    else:
+        # left
+        direction += 1
+        direction %= 4
+    
+    # Infect
+    map[(iA, jA)] = not infected
+    if (not infected):
+        infections += 1
+    
+    # Move
+    # right
+    if (direction == 0):
+        jA += 1
+    # up
+    elif (direction == 1):
+        iA -= 1
+    # left
+    elif (direction == 2):
+        jA -= 1
+    # down
+    elif (direction == 3):
+        iA += 1
+
+print("{0} infections after {1} bursts".format(infections, totalBursts))
--- 22/22b.py
+++ 22/22b.py
@@ -1,3 +1,58 @@
-inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\20\\input20.txt"
+totalBursts = 10000000
+inputfile = "C:\\Users\\jkearns\\Documents\\GitHub\\advent-of-code-2017\\22\\input22.txt"
 with open(inputfile) as f:
     lines = f.readlines()
+
+map = {}
+length = len(lines)
+for i in range(length):
+    for j in range(length):
+        if list(lines[i])[j] == '#':
+            map[(i, j)] = 2
+        else:
+            map[(i, j)] = 0
+
+iA = int(length/2)
+jA = int(length/2)
+direction = 1 # Up
+infections = 0
+for iteration in range(totalBursts):
+    if not (iA, jA) in map:
+        map[(iA, jA)] = 0
+    nodeState = map[(iA, jA)]
+
+    # Turn
+    if nodeState == 0:
+        # clean - left
+        direction += 1
+    elif nodeState == 1:
+        # weakened - no turn
+        direction += 0
+    elif nodeState == 2:
+        # infected - right
+        direction += 0
+    elif nodeState == 3:
+        # flagged - revers
+        direction += 2
+    direction %= 4
+    
+    # Infect
+    map[(iA, jA)] = (nodeState + 1) % 4
+    if (nodeState == 1):
+        infections += 1
+    
+    # Move
+    # right
+    if (direction == 0):
+        jA += 1
+    # up
+    elif (direction == 1):
+        iA -= 1
+    # left
+    elif (direction == 2):
+        jA -= 1
+    # down
+    elif (direction == 3):
+        iA += 1
+
+print("{0} infections after {1} bursts".format(infections, totalBursts))
